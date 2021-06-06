23096193f830b9bb54f8d5dc37650d3a8fddbe43
Chirag-fs <85388034+Chirag-fs@users.noreply.github.com>
2021-06-06 14:51:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import "./styles.css"
+function App() { 
+return  (
+  <div>
+    <Header  />
+    <Footer  />
+    <Note    />
+    <Note    />
+    <Note    />
+  </div>
+ );
+}
+
+export default App;
